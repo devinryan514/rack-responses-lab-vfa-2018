41820f2cabf245ac2b54706237a1bc87c753777b
Devin Ryan <devin.ryan514@gmail.com>
2018-06-28 17:08:53 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
  
   def call(env)
     resp = Rack::Response.new
-    current_time  = Time.now.getlocal.zone
+    current_time  = Time.now.getlocal
     resp.write current_time
     if current_time.hour < 12
       resp.write "Good Morning!"
